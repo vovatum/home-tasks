edeef8c28f588e529a4c174668e439dfa8de0176
vovkahimik <vovkahimik@gmail.com>
2020-04-03 14:47:47 +0300

    FIO

---
+++ src/MyNameIs.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './MyNameIs.css';
+
+function MyNameIs() {
+    return (
+        <div className="MyNameIs">
+        <span>
+            Химиченко Владимир Александрович
+        </span>
+        </div>
+    );
+}
+
+export default MyNameIs;
