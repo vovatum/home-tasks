965ec85d072664a794f12935333a8081799968f5
vovkahimik <vovkahimik@gmail.com>
2020-04-15 18:38:57 +0300

    bck message, qualities-string

--- src/AppComponents/MyNameIs/MyNameIs.jsx
+++ src/AppComponents/MyNameIs/MyNameIs.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import styles from './MyNameIs.module.css';
 
+const qualities = [
+    'Перфекционист',
+    'Сочувствующий',
+    'Нигилист'
+];
+
+let qualitiesSpan = qualities.map(quality => {
+    return <span>{quality}</span>
+});
+
 const MyNameIs = () => {
     return (
-        <div className={styles.myNameIs}>
-            Химиченко Владимир Александрович
+        <div className={styles.container}>
+            <div className={styles.myNameIs}>
+                Химиченко Владимир Александрович
+            </div>
+            <div className={styles.qualities}>
+                {qualitiesSpan}
+            </div>
         </div>
     )
 };
